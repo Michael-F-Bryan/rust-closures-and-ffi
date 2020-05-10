3727eaefaaba3347e51a89f9219526dc36872118
Michael-F-Bryan <Michael-F-Bryan@users.noreply.github.com>
2020-05-10 09:08:38 +0000

    Deploy to GitHub pages

---
+++ search-index.js
@@ -0,0 +1,4 @@
+var searchIndex = JSON.parse('{\
+"rust_closures_and_ffi":{"doc":"","i":[],"p":[]}\
+}');
+addSearchOptions(searchIndex);initSearch(searchIndex);
---
+++ source-files.js
@@ -0,0 +1,3 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["rust_closures_and_ffi"] = {"name":"","files":["lib.rs"]};
+createSourceSidebar();

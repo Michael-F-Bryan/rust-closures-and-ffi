065e6a50896fe5616052ec4825b4eb01a642acd2
Michael-F-Bryan <michaelfbryan@gmail.com>
2020-05-12 21:24:10 +0800

    Added examples using manually written structs and a closure

--- src/better.rs
+++ src/better.rs
@@ -10,3 +10,44 @@ extern "C" {
         user_data: *mut c_void,
     );
 }
+
+pub fn get_trampoline<F>(_closure: &F) -> AddCallback
+where
+    F: FnMut(c_int),
+{
+    trampoline::<F>
+}
+
+unsafe extern "C" fn trampoline<F>(result: c_int, user_data: *mut c_void)
+where
+    F: FnMut(c_int),
+{
+    let user_data = &mut *(user_data as *mut F);
+    user_data(result);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn use_the_trampoline_function() {
+        let mut got = 0;
+
+        {
+            let mut closure = |result: c_int| got = result;
+            let trampoline = get_trampoline(&closure);
+
+            unsafe {
+                better_add_two_numbers(
+                    1,
+                    2,
+                    trampoline,
+                    &mut closure as *mut _ as *mut c_void,
+                );
+            }
+        }
+
+        assert_eq!(got, 1 + 2);
+    }
+}
